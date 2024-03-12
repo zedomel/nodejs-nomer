5a4817f3106172765b804a1b6bf720313852f3fe
Jose Augusto Salim <zedomel@gmail.com>
2024-03-11 22:05:05 -0300

    Save dev

--- src/index.js
+++ src/index.js
@@ -1,10 +1,37 @@
-const JavaCaller = require('java-caller');
-const path = require("path");
+const { exec } = require('child_process')
 
 class Nomer {
 
     constructor() {
 
+    }
+
+    getNomerValidateCmd(filepath="", cmd="validate-term",properties=null) {
+        if (filepath === "") {
+            throw new Exception("Filepath cannot be empty string");
+        }
+        let nomerCmd = `curl -L ${filepath} | nomer ${cmd}`;
+        if (properties) {
+            nomerCmd = `${nomerCmd} -p ${properties}`;
+        }
+
+        return nomerCmd;
+    }
+
+    getNomerMatchCmd(query="",cmd="append",matcher="globi-taxon-cache",properties=null,outputFormat=null,echoOpt="") {
+        let nomerCmd = `echo ${echoOpt} ${query} | nomer ${cmd} ${matcher} -Xmx4096m -Xms1024m`;
+        if (properties) {
+            nomerCmd = `${nomerCmd} -p ${properties}`;
+        }
+        if (outputFormat) {
+            nomerCmd = `${nomerCmd} -o ${outputFormat}`;
+        }
+
+        return nomerCmd;
+    }
+
+    getNomerSimpleCmd(cmd="version", verbose=false, properties=null, outputFormat=null) {
+        
     }
 
     runNomer() {
