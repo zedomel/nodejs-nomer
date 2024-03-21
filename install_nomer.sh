c939d8ef10bf66fa0ac2852b1fcb7d9bea9910c7
José Augusto Salim <jasalim@unicamp.br>
2024-03-21 14:49:33 -0300

    Create bin directory

--- install_nomer.sh
+++ install_nomer.sh
@@ -2,6 +2,7 @@
 
 NOMER_VERSION=$1
 
+mkdir -p ./bin
 echo "https://github.com/globalbioticinteractions/nomer/releases/download/${NOMER_VERSION}/nomer.jar)"
 sh -c "(curl -L https://github.com/globalbioticinteractions/nomer/releases/download/${NOMER_VERSION}/nomer.jar) > ./bin/nomer && chmod +x ./bin/nomer" && \
 	./bin/nomer clean && ./bin/nomer version
