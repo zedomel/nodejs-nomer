d981b21d3ef226bfbbc60398123724d60e99a1fb
José Augusto Salim <zedomel@users.noreply.github.com>
2026-04-02 13:26:37 -0300

    Do not execute nomer (java may not be installed)

--- install_nomer.sh
+++ install_nomer.sh
@@ -4,5 +4,4 @@ NOMER_VERSION="0.6.1"
 
 mkdir -p ./bin
 echo "https://github.com/globalbioticinteractions/nomer/releases/download/${NOMER_VERSION}/nomer.jar)"
-sh -c "(curl -L https://github.com/globalbioticinteractions/nomer/releases/download/${NOMER_VERSION}/nomer.jar) > ./bin/nomer && chmod +x ./bin/nomer" && \
-	./bin/nomer clean && ./bin/nomer version
+sh -c "(curl -L https://github.com/globalbioticinteractions/nomer/releases/download/${NOMER_VERSION}/nomer.jar) > ./bin/nomer && chmod +x ./bin/nomer"
